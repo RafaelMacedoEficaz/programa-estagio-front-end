191b6b933b53e2b17966bbb7160667b4f6cba095
Rafael Macedo <rafaelldemacedo@gmail.com>
2021-03-03 00:47:41 -0300

    Iniciado projeto do front-end

---
+++ resources/layout/menu.php
@@ -0,0 +1,17 @@
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=""><img src="../../imagem/eficaz_logo_black.png" class="img-fluid"></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li class="active">
+                        <a href="index.php"><i class="fa fa-fw fa-dashboard"></i>Cadastro</a>
+                    </li>
+                    <li>
+                        <a href="colecao.php"><i class="fa fa-fw fa-bar-chart-o"></i>Lista de cadastrados</a>
+                    </li>
+                </ul>
+            </div>
